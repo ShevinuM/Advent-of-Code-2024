a4f038f7d27b58e37e3e07211de141a072151eee
ShevinuM <shevinu2002@gmail.com>
2024-12-02 10:42:55 -0330

    Day2 Part2

--- Day2/Day2.go
+++ Day2/Day2.go
@@ -43,28 +43,78 @@ func read() ([][]int, error) {
 func part1(lines [][]int) (int, error) {
 	sum := 0
 	for i := 0; i < len(lines); i++ {
-		isDescending := lines[i][0] > lines[i][1]
-		isValid := true
-		for j := 0; j < len(lines[i])-1; j++ {
-			difference := math.Abs(float64(lines[i][j] - lines[i][j+1]))
-			if difference < 1 || difference > 3 {
-				isValid = false
-				break
-			}
-			if isDescending && lines[i][j] > lines[i][j+1] {
-				continue
-			} else if !isDescending && lines[i][j] < lines[i][j+1] {
-				continue
-			} else {
-				isValid = false
-				break
-			}
+		line := lines[i]
+		isValid, err := check1(line)
+		if err != nil {
+			os.Exit(1)
 		}
 		if isValid {
 			sum += 1
 		}
 	}
 	return sum, nil
+}
+
+func part2(lines [][]int) (int, error) {
+	sum := 0
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+		isValid, err := check1(line)
+		if err != nil {
+			os.Exit(1)
+		}
+		if isValid {
+			sum += 1
+		} else {
+			isValid, err := check2(line)
+			if err != nil {
+				os.Exit(1)
+			}
+			if isValid {
+				sum += 1
+			}
+		}
+	}
+	return sum, nil
+}
+
+func check2(line []int) (bool, error) {
+	for i := 0; i < len(line); i++ {
+		line2 := removeElement(append([]int(nil), line...), i)
+		isValid, err := check1(line2)
+		if err != nil {
+			os.Exit(1)
+		}
+		if isValid {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func check1(line []int) (bool, error) {
+	isDescending := line[0] > line[1]
+	isValid := true
+	for j := 0; j < len(line)-1; j++ {
+		difference := math.Abs(float64(line[j] - line[j+1]))
+		if difference < 1 || difference > 3 {
+			isValid = false
+			break
+		}
+		if isDescending && line[j] > line[j+1] {
+			continue
+		} else if !isDescending && line[j] < line[j+1] {
+			continue
+		} else {
+			isValid = false
+			break
+		}
+	}
+	return isValid, nil
+}
+
+func removeElement(slice []int, index int) []int {
+	return append(slice[:index], slice[index+1:]...)
 }
 
 func main() {
@@ -79,4 +129,10 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("Part 1 -> %d\n", sum)
+	sum2, err := part2(lines)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("Part 2 -> %d\n", sum2)
 }
